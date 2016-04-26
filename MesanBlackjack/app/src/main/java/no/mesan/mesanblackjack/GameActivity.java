cd0251f1ebb107185d36646e43a8b2d412c70bb0
TAD Utvikling <tad@Jorns-MacBook-Pro-2.local>
2016-04-26 23:12:35 +0200

    Removed code and added todos

--- MesanBlackjack/app/src/main/java/no/mesan/mesanblackjack/GameActivity.java
+++ MesanBlackjack/app/src/main/java/no/mesan/mesanblackjack/GameActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 
 import no.mesan.mesanblackjack.adapter.CardAdapter;
@@ -22,7 +21,6 @@ public class GameActivity extends AppCompatActivity {
 
     private RecyclerView dealerRecyclerView;
     private CardAdapter dealerCardAdapter, playerCardAdapter;
-    private Button hitButton, dealButton;
     private LinearLayout resultLayout, gameOverLayout;
 
     public GameActivity() {
@@ -43,40 +41,16 @@ public class GameActivity extends AppCompatActivity {
 
     private void initGui() {
         dealerRecyclerView = (RecyclerView) findViewById(R.id.recyclerView_dealer);
-        // TODO: Oppgave 2a: Initialiser
         setupRecyclerView(dealerRecyclerView);
-        // TODO: Oppgave 2a: Setup spillerens recyclerview
 
-        // TODO: Oppgave 2c: Initialiser
-        // TODO: Oppgave 2d: Initialiser
-        // TODO: Oppgave 2e: Initialiser
-
-        hitButton = (Button)findViewById(R.id.btn_hit);
-        dealButton = (Button)findViewById(R.id.btn_deal);
-        // TODO: Oppgave 2b: Initialiser
+        // TODO: Oppgave 1b:
 
         resultLayout = (LinearLayout)findViewById(R.id.layout_result);
         gameOverLayout = (LinearLayout)findViewById(R.id.layout_gameOver);
     }
 
     private void initListeners() {
-        hitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                playerHits();
-            }
-        });
-
-        // TODO: Oppgave 2b: Sett klikk-lytter
-
-        dealButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dealCards();
-            }
-        });
-
-        // TODO: Oppgave 2e: Sett klikk-lytter
+        // TODO: Oppgave 1d
 
         gameOverLayout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -94,8 +68,6 @@ public class GameActivity extends AppCompatActivity {
         dealerCardAdapter.notifyDataSetChanged();
         playerCardAdapter.notifyDataSetChanged();
 
-        enableDealButtons(true);
-
         gameOverLayout.setVisibility(View.INVISIBLE);
     }
 
@@ -107,16 +79,8 @@ public class GameActivity extends AppCompatActivity {
         playerCardAdapter = new CardAdapter(game.getPlayerHand());
 
         dealerRecyclerView.setAdapter(dealerCardAdapter);
-        // TODO: Oppgave 2a: Sett adapter
-
-        // TODO: Oppgave 2c: Oppdater spillerens score
-        // TODO: Oppgave 2d: Skjul dealerens score
-
-        enableActionButtons(true);
-        enableDealButtons(false);
 
         if (game.playerHasBlackjack()) {
-            enableActionButtons(false);
             playerBlackjack();
         }
     }
@@ -128,17 +92,10 @@ public class GameActivity extends AppCompatActivity {
         dealerCardAdapter.notifyDataSetChanged();
         playerCardAdapter.notifyDataSetChanged();
 
-        // TODO: Oppgave 2c: Reset spillerens score
-        // TODO: Oppgave 2d: Reset dealerens score
-
-        enableActionButtons(false);
-        enableDealButtons(true);
-
         resultLayout.setVisibility(View.INVISIBLE);
 
         if (game.playerHasBlackjack()) {
             playerBlackjack();
-            enableActionButtons(false);
         }
     }
 
@@ -146,14 +103,11 @@ public class GameActivity extends AppCompatActivity {
         game.dealPlayerCard();
 
         playerCardAdapter.notifyDataSetChanged();
-        // TODO: Oppgave 2c: Oppdater spillerens score
 
         if (game.playerHasBlackjack()) {
             playerStands();
-            enableActionButtons(false);
         } else if (game.playerHasBusted()) {
             playerBusts();
-            enableActionButtons(false);
         }
     }
 
@@ -161,84 +115,62 @@ public class GameActivity extends AppCompatActivity {
         game.dealerShowHoleCard();
 
         dealerCardAdapter.notifyDataSetChanged();
-        // TODO: Oppgave 2d: Vis dealerens score
 
         while (game.dealerShouldDrawCard()) {
             game.dealDealerCard();
 
             dealerCardAdapter.notifyDataSetChanged();
-            // TODO: Oppgave 2d: Oppdater dealerens score
 
             if (game.dealerHasBusted()) {
                 dealerBusts();
-                enableActionButtons(false);
                 return;
             }
         }
 
         Game.Outcome outcome = game.getOutcome();
-        enableActionButtons(false);
 
         switch (outcome) {
             case DEALER:
                 playerLoses();
-                enableActionButtons(false);
                 break;
             case PLAYER:
                 playerWins();
-                enableActionButtons(false);
                 break;
             default:
                 draw();
-                enableActionButtons(false);
         }
     }
 
     private void playerBlackjack() {
         game.playerWinBlackjack();
-        // TODO: Oppgave 2e: Oppdater resultat
         showResult();
     }
 
     private void playerWins() {
         game.playerWin();
-        // TODO: Oppgave 2e: Oppdater resultat
         showResult();
     }
 
     private void dealerBusts() {
         game.playerWin();
-        // TODO: Oppgave 2e: Oppdater resultat
         showResult();
     }
 
     private void draw() {
         game.draw();
-        // TODO: Oppgave 2e: Oppdater resultat
         showResult();
     }
 
     private void playerBusts() {
-        // TODO: Oppgave 2e: Oppdater resultat
         showResult();
     }
 
     private void playerLoses() {
-        // TODO: Oppgave 2e: Oppdater resultat
         showResult();
     }
 
     private void showResult() {
         resultLayout.setVisibility(View.VISIBLE);
-    }
-
-    private void enableDealButtons(boolean enabled) {
-        dealButton.setEnabled(enabled);
-    }
-
-    private void enableActionButtons(boolean enable) {
-        hitButton.setEnabled(enable);
-        // TODO: Oppgave 2b: Enable stand button
     }
 
     private void setupRecyclerView(RecyclerView recyclerView) {
